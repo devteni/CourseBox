9411fa05410765a9677ea5160dbaa1013e1f1ea8
devteni <fatunmbiteniola001@gmail.com>
2022-03-28 21:37:45 +0100

    chore: BE setup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,16 +2,21 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-
+import { AuthModule } from './modules/auth/auth.module';
+import { CourseModule } from './modules/course/course.module';
+import { FileModule } from './modules/file/file.module';
+import { StudentModule } from './modules/student/student.module';
+import { AdminModule } from './modules/admin/admin.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    UsersModule,
     AuthModule,
+    AdminModule,
+    CourseModule,
+    FileModule,
+    StudentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
